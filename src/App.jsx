800ddca0ee21d7f44145839f7f0116a8014dbe36
Zoi <zoilanbond@gmail.com>
2023-12-09 09:05:12 -0800

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
 
 function App() {
   const [posts, setPosts] = useState([])
-
+  const [loading, setLoading] = useState(true)
+  
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
       const data = await res.json()
       setPosts(data)
+      setLoading(false)
     }
-
+    
     fetchPosts()
-  }, [])
-
+  },[])
+  
+  
+  if(loading) {
+    return (
+      <div className='container'>
+        <h1>Loading</h1>
+      </div>
+    )
+  }
 
   return (
     <div className='container'>
-      <h1>Test</h1>
+          <h1>We have posts</h1>
     </div>
   )
 }
+
+
 
 export default App
