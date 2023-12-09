7e9d3eaf46b345a7668e23de75b0181090eab8ec
Zoi <zoilanbond@gmail.com>
2023-12-09 08:52:35 -0800

    succesful fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+      const data = await res.json()
+      setPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+
+
   return (
     <div className='container'>
       <h1>Test</h1>
